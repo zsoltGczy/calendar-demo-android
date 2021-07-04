bcd72ec6e04429c888dafdfcfbd3192a0aea2cb0
Zsolt <geczy.zsolt@gmail.com>
2021-07-04 14:11:04 +0200

    Create Calendar Fragment

--- app/src/main/java/com/gzslt/calendardemo/ui/MainActivity.kt
+++ app/src/main/java/com/gzslt/calendardemo/ui/MainActivity.kt
@@ -1,13 +1,19 @@
-package com.gzslt.calendardemo
+package com.gzslt.calendardemo.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.gzslt.calendardemo.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/gzslt/calendardemo/ui/calendar/CalendarFragment.kt
@@ -0,0 +1,30 @@
+package com.gzslt.calendardemo.ui.calendar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gzslt.calendardemo.databinding.FragmentCalendarBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CalendarFragment : Fragment() {
+
+    private var _binding: FragmentCalendarBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCalendarBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
