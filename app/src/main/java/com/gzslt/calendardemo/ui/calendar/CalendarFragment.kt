bb47fbb158ca0ee57d923a40d70ddc6dfbb12263
Zsolt <geczy.zsolt@gmail.com>
2021-07-10 14:12:58 +0200

    Set calendar days

--- app/src/main/java/com/gzslt/calendardemo/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/gzslt/calendardemo/ui/calendar/CalendarFragment.kt
@@ -10,8 +10,16 @@ import androidx.fragment.app.Fragment
 import com.gzslt.calendardemo.R
 import com.gzslt.calendardemo.common.extension.setTextColorRes
 import com.gzslt.calendardemo.databinding.FragmentCalendarBinding
+import com.gzslt.calendardemo.databinding.LayoutCalendarDayBinding
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.model.DayOwner
+import com.kizitonwose.calendarview.ui.DayBinder
+import com.kizitonwose.calendarview.ui.ViewContainer
+import com.kizitonwose.calendarview.utils.yearMonth
 import dagger.hilt.android.AndroidEntryPoint
 import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.YearMonth
 import java.time.format.TextStyle
 import java.util.Locale
 
@@ -20,6 +28,9 @@ class CalendarFragment : Fragment() {
 
     private var _binding: FragmentCalendarBinding? = null
     private val binding get() = _binding!!
+
+    private var selectedDate: LocalDate? = null
+    private val daysOfWeek = DayOfWeek.values()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,10 +45,10 @@ class CalendarFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setLegendLayout()
+        setCalendarDays()
     }
 
     private fun setLegendLayout() {
-        val daysOfWeek = DayOfWeek.values()
         binding.legendLayout.root.children.forEachIndexed { index, dayNameView ->
             (dayNameView as TextView).apply {
                 text =
@@ -47,6 +58,64 @@ class CalendarFragment : Fragment() {
                     ).uppercase()
 
                 setTextColorRes(R.color.legend_day)
+            }
+        }
+    }
+
+    private fun setCalendarDays() {
+        val currentMonth = YearMonth.now()
+        val startMonth = currentMonth.minusMonths(10)
+        val endMonth = currentMonth.plusMonths(10)
+        binding.calendarView.setup(startMonth, endMonth, daysOfWeek.first())
+        binding.calendarView.scrollToMonth(currentMonth)
+
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            lateinit var day: CalendarDay
+            val textView = LayoutCalendarDayBinding.bind(view).dayTextView
+
+            init {
+                view.setOnClickListener {
+                    if (selectedDate == day.date) {
+                        selectedDate = null
+                        binding.calendarView.notifyDayChanged(day)
+                    } else {
+                        if (day.owner != DayOwner.THIS_MONTH) {
+                            binding.calendarView.scrollToDate(day.date)
+                        }
+                        val oldDate = selectedDate
+                        selectedDate = day.date
+                        binding.calendarView.notifyDateChanged(day.date)
+                        oldDate?.let { binding.calendarView.notifyDateChanged(oldDate) }
+                    }
+                }
+            }
+        }
+
+        binding.calendarView.dayBinder = object : DayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                // TODO add event dots
+                container.day = day
+                val textView = container.textView
+                textView.text = day.date.dayOfMonth.toString()
+                if (day.owner == DayOwner.THIS_MONTH) {
+                    when (day.date) {
+                        selectedDate -> {
+                            textView.setBackgroundResource(R.drawable.shape_selected_date_background)
+                        }
+                        LocalDate.now() -> {
+                            textView.setTextColorRes(R.color.today_date)
+                            textView.background = null
+                        }
+                        else -> {
+                            textView.setTextColorRes(R.color.calendar_date_text)
+                            textView.background = null
+                        }
+                    }
+                } else {
+                    textView.setTextColorRes(R.color.calendar_date_text_not_current_month)
+                    textView.background = null
+                }
             }
         }
     }
